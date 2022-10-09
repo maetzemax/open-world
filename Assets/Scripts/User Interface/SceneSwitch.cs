3940f84d9c8508c148bc7e29ee96a19a2ea69c3f
Max Maetze <maetze.max@icloud.com>
2022-10-09 20:40:46 +0200

    implemented some ui placeholders and menu options

---
+++ Assets/Scripts/User Interface/SceneSwitch.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour {
+
+    public void ChangeToSceneWithID(int index) {
+        SceneManager.LoadScene(index);
+    }
+
+}
