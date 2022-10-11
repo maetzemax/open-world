18d71d8411f5e5eb8399482970de1fc288e2ab7e
mmaetze <max.maetze@develappers.de>
2022-10-11 17:07:47 +0200

    adjusted ligthingsystem

--- Assets/Scripts/World/Time Cycle/LightingManager.cs
+++ Assets/Scripts/World/Time Cycle/LightingManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class LigthingManager : MonoBehaviour {
+public class LightingManager : MonoBehaviour {
 
     [SerializeField]
     private Light directionLight;
